131c993f6496054226fab2057cb343fdf3cc06d0
Suvrat1629 <suvrat1629@gmail.com>
2025-09-13 23:02:18 +0530

    first commit

---
+++ src/main/java/com/namaste/Namaste/to/TM2/NamasteToTm2Application.java
@@ -0,0 +1,13 @@
+package com.namaste.Namaste.to.TM2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamasteToTm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamasteToTm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/namaste/Namaste/to/TM2/NamasteToTm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namaste.Namaste.to.TM2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamasteToTm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
